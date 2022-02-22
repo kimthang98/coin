23b0c40fa6406a38fb5b28d170d272e287f63c81
KIMTHANG <thangdk@winds.vn>
2022-02-22 18:07:35 +0700

    pc

--- src/components/mail/service/mail.service.ts
+++ src/components/mail/service/mail.service.ts
@@ -1,8 +1,5 @@
 import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
-import { join } from 'path';
-import * as nodeMailer from 'nodemailer';
-import { sendMail } from './../../../shared/common/sendmail';
 import { ResetPasswordType } from 'src/shared/type/index.type';
 
 @Injectable()
@@ -21,26 +18,5 @@ export class MailService {
         account: user.account,
       },
     });
-  }
-
-  async sendMail() {
-    // const secure = process.env.MAIL_ENCRYPTION == 'false' ? false : true;
-    const transporter = nodeMailer.createTransport({
-      host: `smtp.gmail.com`,
-      port: 587,
-      secure: false,
-      auth: {
-        user: `zocodevelop01@gmail.com`,
-        pass: 'zoco@123456',
-      },
-    });
-    const options = {
-      from: `zocodevelop01@gmail.com`, // địa chỉ admin email bạn dùng để gửi
-      to: 'dokimthang98@gmail.com', // địa chỉ gửi đến
-      subject: 'hello', // Tiêu đề của mail
-      html: `<h1>xin chao</h1>`, // Phần nội dung mail mình sẽ dùng html thay vì thuần văn bản thông thường.
-    };
-
-    return transporter.sendMail(options);
   }
 }
